b77c5972a0d0822c1e8ba69aabc2980c3592d5d4
TonyWu98 <tony.wu@otolawn.com>
2023-03-04 04:37:38 -0500

    app updates

--- APP/common_enums.py
+++ APP/common_enums.py
@@ -6,12 +6,160 @@ class KeyType(enum.IntEnum):
 
 
 class ConsumerKeycodes(enum.IntEnum):
-    VOL_UP = 1
-    VOL_DOWN = 2
-
+    UNASSIGNED              = 0x00
+    POWER                   = 0x30
+    SLEEP                   = 0x32
+    RECORD                  = 0xB2
+    FAST_FORWARD            = 0xB3
+    REWIND                  = 0xB4
+    NEXT                    = 0xB5
+    PREV                    = 0xB6
+    STOP                    = 0xB7
+    PLAY_PAUSE              = 0xCD
+    PAUSE                   = 0xB0
+    VOL_MUTE                = 0xE2
+    VOL_UP                  = 0xE9
+    VOL_DOWN                = 0xEA
+    BRIGHTNESS_UP           = 0x6F
+    BRIGHTNESS_DOWN         = 0x70
 
 class KeyboardKeycodes(enum.IntEnum):
-    a = 1
-    b = 2
-    c = 2
-    d = 2
+    RESERVED        =  0
+    A               =  4
+    B               =  5
+    C               =  6
+    D               =  7
+    E               =  8
+    F               =  9
+    G               = 10
+    H               = 11
+    I               = 12
+    J               = 13
+    K               = 14
+    L               = 15
+    M               = 16
+    N               = 17
+    O               = 18
+    P               = 19
+    Q               = 20
+    R               = 21
+    S               = 22
+    T               = 23
+    U               = 24
+    V               = 25
+    W               = 26
+    X               = 27
+    Y               = 28
+    Z               = 29
+    KEY_1             = 30
+    KEY_2               = 31
+    KEY_3               = 32
+    KEY_4               = 33
+    KEY_5               = 34
+    KEY_6               = 35
+    KEY_7               = 36
+    KEY_8               = 37
+    KEY_9               = 38
+    KEY_0               = 39
+    ENTER           = 40
+    RETURN          = 40
+    ESC             = 41
+    BACKSPACE       = 42
+    TAB             = 43
+    SPACE           = 44
+    MINUS           = 45
+    EQUAL           = 46
+    LEFT_BRACE      = 47
+    RIGHT_BRACE     = 48
+    BACKSLASH       = 49
+    NON_US_NUM      = 50
+    SEMICOLON       = 51
+    QUOTE           = 52
+    TILDE           = 53
+    COMMA           = 54
+    PERIOD          = 55
+    SLASH           = 56
+    CAPS_LOCK       = 0x39
+    F1              = 0x3A
+    F2              = 0x3B
+    F3              = 0x3C
+    F4              = 0x3D
+    F5              = 0x3E
+    F6              = 0x3F
+    F7              = 0x40
+    F8              = 0x41
+    F9              = 0x42
+    F10             = 0x43
+    F11             = 0x44
+    F12             = 0x45
+    PRINT           = 0x46
+    PRINTSCREEN     = 0x46
+    SCROLL_LOCK     = 0x47
+    PAUSE           = 0x48
+    INSERT          = 0x49
+    DELETE          = 0x4C
+    HOME            = 0x4A
+    END             = 0x4D
+    PAGE_UP         = 0x4B
+    PAGE_DOWN       = 0x4E
+    RIGHT_ARROW     = 0x4F
+    LEFT_ARROW      = 0x50
+    DOWN_ARROW      = 0x51
+    UP_ARROW        = 0x52
+    RIGHT           = 0x4F
+    LEFT            = 0x50
+    DOWN            = 0x51
+    UP              = 0x52
+    NUM_LOCK        = 0x53
+    KEYPAD_DIVIDE       = 0x54
+    KEYPAD_MULTIPLY     = 0x55
+    KEYPAD_SUBTRACT     = 0x56
+    KEYPAD_ADD          = 0x57
+    KEYPAD_ENTER        = 0x58
+    KEYPAD_1            = 0x59
+    KEYPAD_2            = 0x5A
+    KEYPAD_3            = 0x5B
+    KEYPAD_4            = 0x5C
+    KEYPAD_5            = 0x5D
+    KEYPAD_6            = 0x5E
+    KEYPAD_7            = 0x5F
+    KEYPAD_8            = 0x60
+    KEYPAD_9            = 0x61
+    KEYPAD_0            = 0x62
+    KEYPAD_DOT          = 0x63
+    KEY_NON_US          = 0x64
+    KEY_APPLICATION     = 0x65
+    KEY_MENU            = 0x65
+
+KeyboardKeycodeStrings = {
+    KeyboardKeycodes.KEY_1.value : "1",
+    KeyboardKeycodes.KEY_2.value : "2",
+    KeyboardKeycodes.KEY_3.value : "3",
+    KeyboardKeycodes.KEY_4.value : "4",
+    KeyboardKeycodes.KEY_5.value : "5",
+    KeyboardKeycodes.KEY_6.value : "6",
+    KeyboardKeycodes.KEY_7.value : "7",
+    KeyboardKeycodes.KEY_8.value : "8",
+    KeyboardKeycodes.KEY_9.value : "9",
+    KeyboardKeycodes.KEY_0.value : "0",
+}
+    
+
+def get_all_key_list():
+    string_list = []
+    string_list += [x.name for x in KeyboardKeycodes]
+    string_list += [x.name for x in ConsumerKeycodes]
+    return string_list
+
+def keycode_to_string(keytype: int, keycode: int):
+
+    if keytype == KeyType.CONSUMER:
+        return ConsumerKeycodes(keycode).name
+    
+    if keytype == KeyType.KEYBOARD:
+        try:
+            return KeyboardKeycodeStrings[keycode]
+        except KeyError:
+            return KeyboardKeycodes(keycode).name
+
+    return "Unsupported Key"
--- APP/configurator.py
+++ APP/configurator.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
-from common_enums import KeyType, KeyboardKeycodes, ConsumerKeycodes
-
-
+from common_enums import KeyType, KeyboardKeycodes, ConsumerKeycodes, keycode_to_string, get_all_key_list
+# import pygame
+import keyboard
 
 class EncoderMap(ttk.Labelframe):
     """Class for an encoder keymap gui
@@ -12,12 +12,14 @@ class EncoderMap(ttk.Labelframe):
         self.configure(text="Encoder")
 
         self.cw_keycode = tk.IntVar()
-        self.cw_keycode.trace_add("write", self.update_cw_label)
+        self.cw_keycode.trace_add("write", self.update_cw_label_callback)
         self.cw_keytype = tk.IntVar()
+        self.cw_keytype.trace_add("write", self.update_cw_label_callback)
 
         self.ccw_keycode = tk.IntVar()
-        self.ccw_keycode.trace_add("write", self.update_ccw_label)
+        self.ccw_keycode.trace_add("write", self.update_ccw_label_callback)
         self.ccw_keytype = tk.IntVar()
+        self.ccw_keytype.trace_add("write", self.update_cw_label_callback)
         
         self.label_cw = ttk.Label(self, text="↻CW")
         self.label_cw.grid(row=1, column=1)
@@ -35,16 +37,17 @@ class EncoderMap(ttk.Labelframe):
         self.btn_ccw_edit.grid(row=2, column=3)
 
         # init keycodes to invoke trace callback
-        self.cw_keycode.set(1)
-        self.ccw_keycode.set(2)
+        # self.cw_keycode.set(0)
+        # self.cw_keytype.set(0)
+        # self.ccw_keycode.set(0)
+        # self.ccw_keytype.set(0)
 
     
-    def update_cw_label(self, var: str, index: str, mode: str):
-        self.label_cw_key.configure(text=ConsumerKeycodes(self.cw_keycode.get()).name)
+    def update_cw_label_callback(self, var: str, index: str, mode: str):
+        self.label_cw_key.configure(text=keycode_to_string(self.cw_keytype.get(), self.cw_keycode.get()))
     
-    def update_ccw_label(self, var: str, index: str, mode: str):
-        self.label_ccw_key.configure(text=ConsumerKeycodes(self.ccw_keycode.get()).name)
-
+    def update_ccw_label_callback(self, var: str, index: str, mode: str):
+        self.label_ccw_key.configure(text=keycode_to_string(self.ccw_keytype.get(), self.ccw_keycode.get()))
 
 class KeyMap(ttk.Labelframe):
     """Class for an encoder keymap gui
@@ -53,19 +56,35 @@ class KeyMap(ttk.Labelframe):
         super().__init__(master, *args, **kwargs)
 
         self.keycode = tk.IntVar()
-        self.keycode.trace_add("write", self.update_label)
+        self.keycode.trace_add("write", self.update_label_callback)
         self.keytype = tk.IntVar()
+        self.keytype.trace_add("write", self.update_label_callback)
 
         self.label_key = ttk.Label(self, text="None")
-        self.label_key.grid(row=1, column=1)
+        self.label_key.pack()
         self.btn_edit = ttk.Button(self, text="Edit")
-        self.btn_edit.grid(row=2, column=1)
+        self.btn_edit.pack()
 
-        self.keycode.set(97)
+        # self.keycode.set(0)
 
-    def update_label(self, var: str, index: str, mode: str):
-        self.label_key.configure(text=chr(self.keycode.get()))
-        
+    def update_label_callback(self, var: str, index: str, mode: str):
+        self.label_key.configure(text=keycode_to_string(self.keytype.get(), self.keycode.get()))
+
+
+class MapEditFrame(ttk.Labelframe):
+    """Represents a keymap edit section"""
+
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.configure(text="Edit")
+
+        self.label_a = ttk.Label(self, text="Press key to set...")
+        self.label_a.grid(row=1, column=1)
+
+        self.label_key = ttk.Combobox(self, text="None")
+        self.label_key.grid(row=1, column=2)
+        self.label_key['values'] = get_all_key_list()
+
 
 class KeymapFrame(ttk.Frame):
     """Represents a the keymap tab
@@ -75,21 +94,38 @@ class KeymapFrame(ttk.Frame):
         super().__init__(master, *args, **kwargs)
 
         # Create encoder map
-        self.labelframe_encoder = EncoderMap(self)
-        self.labelframe_encoder.grid(row=1, column=1, columnspan=2)
+        self.lf_encoder = EncoderMap(self)
+        self.lf_encoder.grid(row=1, column=1, columnspan=2)
+        self.lf_encoder.ccw_keytype.set(KeyType.CONSUMER.value)
+        self.lf_encoder.cw_keycode.set(ConsumerKeycodes.VOL_UP.value)
+        self.lf_encoder.cw_keytype.set(KeyType.CONSUMER.value)
+        self.lf_encoder.ccw_keycode.set(ConsumerKeycodes.VOL_DOWN.value)
 
         # Create key map
         self.lf_key1 = KeyMap(self, text="Digital Key 1")
         self.lf_key1.grid(row=2, column=1)
+        self.lf_key1.keytype.set(KeyType.KEYBOARD.value)
+        self.lf_key1.keycode.set(KeyboardKeycodes.A.value)
         
         self.lf_key2 = KeyMap(self, text="Digital Key 2")
         self.lf_key2.grid(row=2, column=2)
+        self.lf_key2.keytype.set(KeyType.KEYBOARD.value)
+        self.lf_key2.keycode.set(KeyboardKeycodes.S.value)
 
         self.lf_key3 = KeyMap(self, text="Analog Key 1")
         self.lf_key3.grid(row=3, column=1)
+        self.lf_key3.keytype.set(KeyType.KEYBOARD.value)
+        self.lf_key3.keycode.set(KeyboardKeycodes.Z.value)
         
         self.lf_key4 = KeyMap(self, text="Analog Key 2")
         self.lf_key4.grid(row=3, column=2)
+        self.lf_key4.keytype.set(KeyType.KEYBOARD.value)
+        self.lf_key4.keycode.set(KeyboardKeycodes.X.value)
+
+        # Create Map Edit Frame
+        self.lf_mapedit = MapEditFrame(self)
+        self.lf_mapedit.grid(row=4, column=1, columnspan=2)
+
 
 class SettingsFrame(ttk.Frame):
     """Represents the settings tab"""
@@ -107,7 +143,7 @@ class Application(ttk.Frame):
         super().__init__(master=master)
         # self.configure(background="green")
 
-        self.btn_upload = tk.Button(self, text="Upload")
+        self.btn_upload = ttk.Button(self, text="Upload")
         self.btn_upload.grid(row=1, column=1)
         
         self.notebook = ttk.Notebook(self)
@@ -127,10 +163,18 @@ class Application(ttk.Frame):
         print(event.keysym)
 
 
-root = tk.Tk()
-# root.geometry("200x600")
-root.title("FLUXPAD Config")
-app = Application(master=root)
-app.pack()
-root.bind("<KeyPress>", func=app.key_test)
-app.mainloop()
+def keyboard_callback(event: keyboard.KeyboardEvent):
+    print(event, event.scan_code, event.scan_code)
+
+
+if __name__ == "__main__":
+
+    keyboard.hook(callback=keyboard_callback)
+
+    root = tk.Tk()
+    # root.geometry("200x600")
+    root.title("FLUXPAD Config")
+    app = Application(master=root)
+    app.pack()
+    # root.bind("<Key>", func=app.key_test)
+    app.mainloop()
